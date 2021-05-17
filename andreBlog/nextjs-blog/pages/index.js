5e6fdc62c44875908b2f0623204c49824d2f26c2
andre <andre_w3@yahoo.com>
2021-05-16 20:46:58 -0400

    added getStaticProps to make  blog

--- andreBlog/nextjs-blog/pages/index.js
+++ andreBlog/nextjs-blog/pages/index.js
@@ -3,7 +3,18 @@ import Link from 'next/link'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 
-export default function Home() {
+import { getSortedPostsData } from '../lib/posts'
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
+
+export default function Home({allPostsData}) {
   return (
     <Layout home>
       <Head>
@@ -15,9 +26,23 @@ export default function Home() {
           (This is a sample website - you’ll be building a site like this on{' '})
         </p>
       </section>
-      <h2>
-        Blog
-      </h2>
+ 
+
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+      <h2 className={utilStyles.headingLg}>Blog</h2>
+      <ul className={utilStyles.list}>
+        {allPostsData.map(({ id, date, title }) => (
+          <li className={utilStyles.listItem} key={id}>
+            {title}
+            <br />
+            {id}
+            <br />
+            {date}
+          </li>
+        ))}
+      </ul>
+    </section>
+
       <Link href="/posts/first-post">
           <a>Here goes nothing</a>
       </Link>
