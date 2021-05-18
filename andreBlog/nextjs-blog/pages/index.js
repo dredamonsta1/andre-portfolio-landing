df0965aa1b607df820f313d9395bd08904471a8c
andre <andre_w3@yahoo.com>
2021-05-18 04:51:29 -0400

    added dynimic links

--- andreBlog/nextjs-blog/pages/index.js
+++ andreBlog/nextjs-blog/pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Link from 'next/link'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
+import Date from '../components/date'
 
 import { getSortedPostsData } from '../lib/posts'
 
@@ -18,26 +19,22 @@ export default function Home({allPostsData}) {
         (This is a sample website - you’ll be building a site like this on{' '})
         </p>
       </section>
-      
-
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
       <h2 className={utilStyles.headingLg}>Blog</h2>
       <ul className={utilStyles.list}>
         {allPostsData.map(({ id, date, title }) => (
           <li className={utilStyles.listItem} key={id}>
-            {title}
-            <br />
-            {id}
-            <br />
-            {date}
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
             ))}
             </ul>
-            </section>
-
-            <Link href="/posts/first-post">
-          <a>Here goes nothing</a>
-          </Link>
+            </section>     
           </Layout>
           )
         }
