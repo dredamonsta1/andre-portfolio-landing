f3cf7d5b18820d5c824c0055999d31be4434e6d1
andre <andre_w3@yahoo.com>
2021-06-28 00:11:56 -0400

    refactoring to dry up code

--- index.js
+++ index.js
@@ -13,25 +13,36 @@ let modalClose = document.querySelector(".modal-close");
 
 
 
+[
+    document.querySelector(".intro"), 
+    document.querySelector(".work"),
+    document.querySelector(".about"),
+    document.querySelector(".contact")
+].forEach(item => {
+    
+    item.addEventListener("click", function(){
+        modalBg.classList.add("bg-active");
+    })
+})
 
-modalBtn.addEventListener('click', function(){
-    modalBg.classList.add("bg-active");
-});
+// modalBtn.addEventListener('click', function(){
+//     modalBg.classList.add("bg-active");
+// });
 
 modalClose.addEventListener("click", function(){
     modalBg.classList.remove("bg-active");
 });
 
 
-workModalBtn.addEventListener("click", function(){
-    modalBg.classList.add("bg-active");
-});
+// workModalBtn.addEventListener("click", function(){
+//     modalBg.classList.add("bg-active");
+// });
 
-aboutModalBtn.addEventListener("click", function(){
-    modalBg.classList.add("bg-active");
-});
+// aboutModalBtn.addEventListener("click", function(){
+//     modalBg.classList.add("bg-active");
+// });
 
 
-contactModalBtn.addEventListener("click", function(){
-    modalBg.classList.add("bg-active");
-});
+// contactModalBtn.addEventListener("click", function(){
+//     modalBg.classList.add("bg-active");
+// });
