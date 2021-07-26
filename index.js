cdece7c620db1da41a51b4cc288f319c99bad95b
andre <andre_w3@yahoo.com>
2021-07-26 11:29:57 -0400

    working on different modal views and favicon

--- index.js
+++ index.js
@@ -10,45 +10,71 @@
 
 
 let modalBgIntro = document.querySelector(".modal-bg");
-let modalBgWork = document.querySelector(".modal-bg-work");
-let modalBgAbout = document.querySelector(".modal-bg-about");
-let modalBgContact = document.querySelector(".modal-bg-contact");
+// let modalBgWork = document.querySelector(".modal-bg-work");
+// let modalBgAbout = document.querySelector(".modal-bg-about");
+// let modalBgContact = document.querySelector(".modal-bg-contact");
 
-let modalClose = document.querySelector(".modal-close");
+// let modalClose = document.querySelector(".modal-close");
+
+// *********************************NEW CODE*************
+var modal = document.getElementById("introModal");
+
+var introBtn = document.getElementById("introModalBtn");
+
+var span = document.getElementsByClassName("modal-close")[0];
 
 
 
-[
-    document.querySelector(".intro"), 
-    document.querySelector(".work"),
-    document.querySelector(".about"),
-    document.querySelector(".contact")
-].forEach(item => {
+introBtn.onclick = function() {
+    modal.style.display = "block";
+    console.log("you clicked")
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+    console.log("x got clicked")
+  }
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+
+// [
+//     document.querySelector(".intro"), 
+//     document.querySelector(".work"),
+//     document.querySelector(".about"),
+//     document.querySelector(".contact")
+// ].forEach(item => {
     
-    item.addEventListener("click", function(){
-        modalBgIntro.classList.add("bg-active");
+//     item.addEventListener("click", function(modalInfo){
+//         modalBgIntro.classList.add("bg-active");
         // modalBgAbout.classList.add("bg-active");
         // modalBgIntro.classList.add("bg-active");
+        // return 
 
-    })
-});
+//     })
+// });
 
 
 
-modalClose.addEventListener("click", function(){
-    modalBgIntro.classList.remove("bg-active");
+// modalClose.addEventListener("click", function(){
+//     modalBgIntro.classList.remove("bg-active");
     // modalBgAbout.classList.remove("bg-active");
     // modalBgContact.classList.remove("bg-active");
-});
+// });
 
 
-function modalInfo() {
-    return 
-    let introModal = modalBgIntro.classList.add("bg-active");
+// function modalInfo() {
+//     return 
+//     let introModal = modalBgIntro.classList.add("bg-active");
     
-    let workModal = modalBgWork.classList.add("bg-active");
+//     let workModal = modalBgWork.classList.add("bg-active");
     
-    let aboutModal = modalBgAbout.classList.add("bg-active");
+//     let aboutModal = modalBgAbout.classList.add("bg-active");
     
-    let contactModal = modalBgContact.classList.add("bg-active");
-}
+//     let contactModal = modalBgContact.classList.add("bg-active");
+// }
