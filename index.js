796cfbc9047a0bac1d019b578ff535e41540f7ac
andre <andre_w3@yahoo.com>
2021-07-29 17:02:23 -0400

    buttons are working but need to refactor

--- index.js
+++ index.js
@@ -6,117 +6,8 @@
 //     modal.style.display = "none";
 //   }
 // }
-
-
-
-// let modalBgIntro = document.querySelector(".modal-bg");
-// let modalBgWork = document.querySelector(".modal-bg-work");
-// let modalBgAbout = document.querySelector(".modal-bg-about");
-// let modalBgContact = document.querySelector(".modal-bg-contact");
-
-// let modalClose = document.querySelector(".modal-close");
-
 // *********************************NEW CODE*************
 
-// get modal element
-// var modal = document.getElementById('introModal');
-
-// get open modal
-// var introBtn = document.getElementById('introModalBtn');
-
-// get close button
-// var closeBtn = document.getElementsByClassName('modal-close')[0];
-
-// listen for open click
-// introBtn.addEventListener('click', openModal);
-
-// listen for close click
-// introBtn.addEventListener('click', closeModal);
-
-// listen for outside click
-// window.addEventListener('click', clickOutside)
-
-// function to open modal
-// function openModal() {
-//   modal.style.display = 'flex';
-//   console.log('123')
-
-// }
-
-// function changeColor(newColor) {
-//   var elem = document.getElementById("introModal");
-//   elem.style.display = newColor;
-//   console.log("jump")
-// }
-
-// function to close modal
-// function closeModal() {
-//   modal.style.display = 'none';
-//   console.log('end')
-
-// }
-
-// function to close modal if outside click
-// function clickOutside() {
-//   if(event.target == modal){
-//     modal.style.display = 'none';
-//   }
-
-// }
-
-// introBtn.onclick = function() {
-//     modal[0].style.display = "block";
-//     console.log("you clicked")
-// }
-
-// When the user clicks on <span> (x), close the modal
-// span.onclick = function() {
-//     modal.style.display = "none";
-//     console.log("x got clicked")
-//   }
-
-// When the user clicks anywhere outside of the modal, close it
-// window.onclick = function(event) {
-//     if (event.target == modal) {
-//       modal.style.display = "none";
-//     }
-//   }
-
-// [
-//     document.querySelector(".intro"), 
-//     document.querySelector(".work"),
-//     document.querySelector(".about"),
-//     document.querySelector(".contact")
-// ].forEach(item => {
-    
-//     item.addEventListener("click", function(modalInfo){
-//         modalBgIntro.classList.add("bg-active");
-        // modalBgAbout.classList.add("bg-active");
-        // modalBgIntro.classList.add("bg-active");
-        // return 
-
-//     })
-// });
-
-
-
-// modalClose.addEventListener("click", function(){
-//     modalBgIntro.classList.remove("bg-active");
-    // modalBgAbout.classList.remove("bg-active");
-    // modalBgContact.classList.remove("bg-active");
-// });
-
-
-// function modalInfo() {
-//     return 
-//     let introModal = modalBgIntro.classList.add("bg-active");
-    
-//     let workModal = modalBgWork.classList.add("bg-active");
-    
-//     let aboutModal = modalBgAbout.classList.add("bg-active");
-    
-//     let contactModal = modalBgContact.classList.add("bg-active");
-// }
 
 let modal = document.getElementById('introModal')
 let modalWork = document.getElementById('workModal')
@@ -130,6 +21,14 @@ let openWork = document.getElementById('workModalBtn')
 let openAbout = document.getElementById('aboutModalBtn')
 
 let openContact = document.getElementById('contactModalBtn')
+
+let close = document.getElementsByClassName('modal-close')[0];
+
+let closeWork = document.getElementsByClassName('modal-close')[1];
+
+let closeAbout = document.getElementsByClassName('modal-close')[2];
+
+let closeContact = document.getElementsByClassName('modal-close')[3];
 
 openIntro.addEventListener('click', (event) => {
   if (
@@ -184,11 +83,7 @@ openContact.addEventListener('click', (event) => {
     )
     {
     return modalContact.classList.add("bg-active");
-    
-    
-    // modal.style.display = "none";
   }
-  console.log('did it work')
 })
 
 
@@ -200,13 +95,50 @@ close.addEventListener('click', (event) => {
     event.target !== modalContact
     )
     {
-    return console.log(`did it: ${span}`, event.path)
+    return modal.classList.remove("bg-active"); 
     
-    modal.classList.remove("bg-active");
-    
-    
-    
-    modal.style.display = "none";
   }
   console.log('did it work')
+})
+
+closeWork.addEventListener('click', (event) => {
+  if (
+    event.target !== modal && 
+    event.target !== modalWork && 
+    event.target !==modalAbout && 
+    event.target !== modalContact
+    )
+    {
+    return modalWork.classList.remove("bg-active");
+    console.log('did it work')
+    
+  }
+})
+
+closeAbout.addEventListener('click', (event) => {
+  if (
+    event.target !== modal && 
+    event.target !== modalWork && 
+    event.target !==modalAbout && 
+    event.target !== modalContact
+    )
+    {
+    return modalAbout.classList.remove("bg-active");
+    console.log('did it work')
+    
+  }
+})
+
+closeContact.addEventListener('click', (event) => {
+  if (
+    event.target !== modal && 
+    event.target !== modalWork && 
+    event.target !==modalAbout && 
+    event.target !== modalContact
+    )
+    {
+    return modalContact.classList.remove("bg-active");
+    console.log('did it work')
+    
+  }
 })
