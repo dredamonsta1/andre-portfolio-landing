0bcdf5d27ad1a4d8614dbafea140d388b8d28420
andre <andre_w3@yahoo.com>
2021-07-29 01:07:24 -0400

    working on different modal views btns

--- index.js
+++ index.js
@@ -9,7 +9,7 @@
 
 
 
-let modalBgIntro = document.querySelector(".modal-bg");
+// let modalBgIntro = document.querySelector(".modal-bg");
 // let modalBgWork = document.querySelector(".modal-bg-work");
 // let modalBgAbout = document.querySelector(".modal-bg-about");
 // let modalBgContact = document.querySelector(".modal-bg-contact");
@@ -19,37 +19,48 @@ let modalBgIntro = document.querySelector(".modal-bg");
 // *********************************NEW CODE*************
 
 // get modal element
-var modal = document.getElementById("introModal");
+// var modal = document.getElementById('introModal');
 
 // get open modal
-var introBtn = document.getElementById("introModalBtn");
+// var introBtn = document.getElementById('introModalBtn');
 
 // get close button
-var closeBtn = document.getElementsByClassName("modal-close")[0];
+// var closeBtn = document.getElementsByClassName('modal-close')[0];
 
 // listen for open click
-introBtn.addEventListener('click', changeColor);
+// introBtn.addEventListener('click', openModal);
 
 // listen for close click
 // introBtn.addEventListener('click', closeModal);
 
+// listen for outside click
+// window.addEventListener('click', clickOutside)
+
 // function to open modal
-function openModal() {
-  modal.style.display = 'block';
-  console.log('123')
+// function openModal() {
+//   modal.style.display = 'flex';
+//   console.log('123')
 
-}
+// }
 
-function changeColor(newColor) {
-  var elem = document.getElementById("introModal");
-  elem.style.display = newColor;
-  console.log("jump")
-}
+// function changeColor(newColor) {
+//   var elem = document.getElementById("introModal");
+//   elem.style.display = newColor;
+//   console.log("jump")
+// }
 
 // function to close modal
 // function closeModal() {
 //   modal.style.display = 'none';
 //   console.log('end')
+
+// }
+
+// function to close modal if outside click
+// function clickOutside() {
+//   if(event.target == modal){
+//     modal.style.display = 'none';
+//   }
 
 // }
 
@@ -106,3 +117,15 @@ function changeColor(newColor) {
     
 //     let contactModal = modalBgContact.classList.add("bg-active");
 // }
+
+let modal = document.querySelector('modal-bg')
+let modalWork = document.querySelector('modal-bg-work')
+let modalAbout = document.querySelector('modal-bg-about')
+let modalContact = document.querySelector('modal-bg-contact')
+
+window.addEventListener('click', event => {
+  if (event.target !== modal && event.target !== modalWork && event.target !==modalAbout && event.target !== modalContact){
+    return console.log(`did it ${event.path}`, event.path)
+  }
+  console.log('did it work')
+})
