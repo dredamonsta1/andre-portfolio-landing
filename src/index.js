d0da144255248123fe877f8a8e104433d3a8e93f
andre <andre_w3@yahoo.com>
2021-10-03 15:00:48 -0400

    setting up react

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App.js";
-ReactDOM.render(<App />, document.getElementById("root"));
+import './App.css';
+// import img from './image/IMG_5161.JPG';
+
+
+ReactDOM.render(
+
+    <App />
+    
+    ,document.getElementById("root")
+);
