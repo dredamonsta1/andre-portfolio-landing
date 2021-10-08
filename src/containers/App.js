951982913bad5a6a42557f54564900507754a397
andre <andre_w3@yahoo.com>
2021-10-08 12:09:03 -0400

    background fixed

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,12 +3,12 @@ import {hot} from "react-hot-loader";
 import LandingPageCircle from "../components/LandingPageCircle/LandingPageCircle";
 import LandingPageContainer from "../components/LandigPageContainer/LandingPageContainer";
 import Modal from "../components/Modal/Modal";
-import img from '../image/aurora.jpg';
+import img from '../image/nyc.JPG';
 import "./App.css";
 
 function App() {
     return(
-      <div className="app-background" styles={{ backgroundImage:'url(' +img + ')' }}>
+      <div className="app-background" style={{ backgroundImage:`url(${img} )` }}>
       
 
         <LandingPageCircle />
