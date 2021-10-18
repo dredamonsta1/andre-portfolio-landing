3926f10212ff4adc38113bf5359de1726354f003
andre <andre_w3@yahoo.com>
2021-10-18 00:42:26 -0400

    adding icons

--- src/components/LandingPageFooter/LandingPageFooter.js
+++ src/components/LandingPageFooter/LandingPageFooter.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { faCoffee, faTwitter, faLinkedin, faFacebookF } from '@fortawesome/free-solid-svg-icons'
 // import Modal from '../Modal/Modal';
 
 
@@ -9,18 +9,23 @@ import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 function LandingPageFooter() {
 
 
-    const element = <FontAwesomeIcon icon={faCoffee} />
+    const element = <FontAwesomeIcon icon={faCoffee} rotation={90}/>
+    const twitter = <FontAwesomeIcon icon={faTwitter} />
+    const linkedin = <FontAwesomeIcon icon={faFacebookF} />
+    
+    
 
     return (
         <div className="contact-icons">
         
         
         
-        <a href="https://www.linkedin.com/in/andre-wilkinson/"><i className="fa fa-linkedin"></i>linkedin</a>
-        <a href="https://www.youtube.com/watch?v=6Us5s49NBwY">youtube {element}</a>
+        <a href="https://www.linkedin.com/in/andre-wilkinson/">{element}</a>
+        <a href="https://www.youtube.com/watch?v=6Us5s49NBwY">{element}</a>
         <a href="https://www.ninebyfour.tech/"><i className="fa fa-laptop">Website</i></a>
         <a href="https://github.com/dredamonsta1"><i className="fa fa-github"></i>github</a>
-        <a href="https://twitter.com/dredamonsta1"><i className="fa fa-twitter"></i>twitter</a>
+        <a href="https://twitter.com/dredamonsta1">{linkedin}</a>
+        <div><i className="fa fa-spinner fa-spin">no spinner but why</i></div>;
         
         </div>
         ); 
