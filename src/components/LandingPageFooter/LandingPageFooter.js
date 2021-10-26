adbe8ca10151acfac4ee146cbdf4e58d7d8fe4da
andre <andre_w3@yahoo.com>
2021-10-26 12:55:37 -0400

    cleaned up some code

--- src/components/LandingPageFooter/LandingPageFooter.js
+++ src/components/LandingPageFooter/LandingPageFooter.js
@@ -5,23 +5,23 @@ import React from 'react';
 
 function LandingPageFooter() {
 
-    let url="https://www.linkedin.com/in/andre-wilkinson/";
-    let urlw="https://www.ninebyfour.tech/";
-    let urlg="https://github.com/dredamonsta1";
-    let urlt="https://twitter.com/dredamonsta1";
-    let element =<a href="urll">Linkedin</a>;
-    let elementWeb =<a href="urlw">Website</a>;
-    let elementLink =<a href="urlg">Github</a>;
-    let elementTwit =<a href="urlt">Twitter</a>;
+    // let url="https://www.linkedin.com/in/andre-wilkinson/";
+    // let urlw="https://www.ninebyfour.tech/";
+    // let urlg="https://github.com/dredamonsta1";
+    // let urlt="https://twitter.com/dredamonsta1";
+    // let element =<a href="urll">Linkedin</a>;
+    // let elementWeb =<a href="urlw">Website</a>;
+    // let elementLink =<a href="urlg">Github</a>;
+    // let elementTwit =<a href="urlt">Twitter</a>;
     // let element =<a href="urll">Linkedin</a>;
     return (
         <div className="contact-icons">
         
-        <a href="https://www.linkedin.com/in/andre-wilkinson/"><i className="fa fa-linkedin"></i>linkedin</a>
-        <a href="https://www.youtube.com/watch?v=6Us5s49NBwY">youtube</a>
-        <a href="https://www.ninebyfour.tech/"><i className="fa fa-laptop">Website</i></a>
-        <a href="https://github.com/dredamonsta1"><i className="fa fa-github"></i>github</a>
-        <a href="https://twitter.com/dredamonsta1"><i className="fa fa-twitter"></i>twitter</a>
+            <a href="https://www.linkedin.com/in/andre-wilkinson/"><i className="fa fa-linkedin"></i>linkedin</a>
+            <a href="https://www.youtube.com/watch?v=6Us5s49NBwY">youtube</a>
+            <a href="https://www.ninebyfour.tech/"><i className="fa fa-laptop">Website</i></a>
+            <a href="https://github.com/dredamonsta1"><i className="fa fa-github"></i>github</a>
+            <a href="https://twitter.com/dredamonsta1"><i className="fa fa-twitter"></i>twitter</a>
         
         </div>
     ); 
