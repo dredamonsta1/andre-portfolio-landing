733e44980c4c84f4a3144299e4cd658bf9ce047f
andre <andre_w3@yahoo.com>
2021-10-26 14:44:49 -0400

    coffee mugs

--- src/components/LandingPageFooter/LandingPageFooter.js
+++ src/components/LandingPageFooter/LandingPageFooter.js
@@ -1,27 +1,23 @@
 import React from 'react';
-// import Modal from '../Modal/Modal';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
 
 
 function LandingPageFooter() {
 
-    // let url="https://www.linkedin.com/in/andre-wilkinson/";
-    // let urlw="https://www.ninebyfour.tech/";
-    // let urlg="https://github.com/dredamonsta1";
-    // let urlt="https://twitter.com/dredamonsta1";
-    // let element =<a href="urll">Linkedin</a>;
-    // let elementWeb =<a href="urlw">Website</a>;
-    // let elementLink =<a href="urlg">Github</a>;
-    // let elementTwit =<a href="urlt">Twitter</a>;
-    // let element =<a href="urll">Linkedin</a>;
+    
+    const element = <FontAwesomeIcon icon={faCoffee} />
+
+
     return (
         <div className="contact-icons">
         
-            <a href="https://www.linkedin.com/in/andre-wilkinson/"><i className="fa fa-linkedin"></i>linkedin</a>
-            <a href="https://www.youtube.com/watch?v=6Us5s49NBwY">youtube</a>
-            <a href="https://www.ninebyfour.tech/"><i className="fa fa-laptop">Website</i></a>
-            <a href="https://github.com/dredamonsta1"><i className="fa fa-github"></i>github</a>
-            <a href="https://twitter.com/dredamonsta1"><i className="fa fa-twitter"></i>twitter</a>
+            <a href="https://www.linkedin.com/in/andre-wilkinson/"><i className="fa fa-linkedin"></i>linkedin{element}</a>
+            <a href="https://www.youtube.com/watch?v=6Us5s49NBwY">{element}youtube{element}</a>
+            <a href="https://www.ninebyfour.tech/"><i className="fa fa-laptop">Website{element}</i></a>
+            <a href="https://github.com/dredamonsta1"><i className="fa fa-github"></i>{element}github</a>
+            <a href="https://twitter.com/dredamonsta1"><i className="fa fa-twitter"></i>{element}twitter{element}</a>
         
         </div>
     ); 
