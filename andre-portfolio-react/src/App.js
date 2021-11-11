c8328f4191b7b79747b6985d2ec444a265f73756
andre <andre_w3@yahoo.com>
2021-11-11 01:50:33 -0500

    remaking landing page

--- andre-portfolio-react/src/App.js
+++ andre-portfolio-react/src/App.js
@@ -1,9 +1,30 @@
+import img from '../src/image/nyc.JPG';
+import LandingPageCircle from './components/LandingPageCircle/LandingPageCircle';
+import LandingPageContainer from './components/LandigPageContainer/LandingPageContainer';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div 
+      className="app-background"
+      style={{
+      backgroundImage: `url(${img})`,
+      backgroundRepeat: "no-repeat",
+      width: '100%',
+      height: '100%'
+      }}>
+      <div className="filter-shade">
+        <div className="small-container">
         <h1> Hello world</h1>
+          <LandingPageCircle />
+          <LandingPageContainer />
+      
+        
+        
+        
+        
+        </div>
+      </div>
     </div>
   );
 }
