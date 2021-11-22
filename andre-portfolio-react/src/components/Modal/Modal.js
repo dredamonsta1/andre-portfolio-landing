5763ecf974de0d715d2a9e0af8f59e2952d688fb
andre <andre_w3@yahoo.com>
2021-11-22 02:32:47 -0500

    working on objects inside modal

--- andre-portfolio-react/src/components/Modal/Modal.js
+++ andre-portfolio-react/src/components/Modal/Modal.js
@@ -10,9 +10,9 @@ const Modal = ({
         <div className={styles.listWork}>
             <div className={styles.modal}>
                 <div className={styles.intro}>
-                    <header className={styles.modalHeader}>
-                        <h2 className={styles.modalTitle}>INTRO</h2>
-                        <button claseName={styles.close}>Close</button>
+                <header className={styles.modalHeader}>
+                <h2 className={styles.modalTitle}>INTRO</h2>
+                <button claseName={styles.topClose}>X</button>
                     </header>
                     <main className={styles.modalContent}>This is Modal Content</main>
                     <footer className={styles.modalFooter}>
@@ -26,7 +26,7 @@ const Modal = ({
         
         : null}</>;
         
-    };
+};
     
     
     export default Modal;
