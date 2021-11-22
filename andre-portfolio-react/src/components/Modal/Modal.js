4618f7fc1c9159b6e1b3f47fe0fc538a78992c0e
andre <andre_w3@yahoo.com>
2021-11-22 03:25:59 -0500

    added modal to a portal element

--- andre-portfolio-react/src/components/Modal/Modal.js
+++ andre-portfolio-react/src/components/Modal/Modal.js
@@ -1,28 +1,32 @@
-import React from "react";
+import ReactDOM from "react-dom";
 import styles from "./Modal.module.css";
 
 
 
 const Modal = ({show, close, title, children}) => {
-    return <> {show ? 
-        <div className={styles.listWork} onClick={() => close()}>
-            <div className={styles.modal} onClick={(e) => e.stopPropagation()}>
-                <div className={styles.intro}>
-                <header className={styles.modalHeader}>
-                <h2 className={styles.modalTitle}>{title}</h2>
-                <button claseName={styles.topClose} onClick={() => close()}>X</button>
-                    </header>
-                    <main className={styles.modalContent}>{children}</main>
-                    <footer className={styles.modalFooter}>
-                        <button className={styles.modalClose} onClick={() => close()}>Close</button>
-                        <button className={styles.submit}>submit</button>
-
-                    </footer>
+    return ReactDOM.createPortal(
+        <> {show ? 
+            <div className={styles.listWork} onClick={() => close()}>
+                <div className={styles.modal} onClick={(e) => e.stopPropagation()}>
+                    <div className={styles.intro}>
+                    <header className={styles.modalHeader}>
+                    <h2 className={styles.modalTitle}>{title}</h2>
+                    <button claseName={styles.topClose} onClick={() => close()}>X</button>
+                        </header>
+                        <main className={styles.modalContent}>{children}</main>
+                        <footer className={styles.modalFooter}>
+                            <button className={styles.modalClose} onClick={() => close()}>Close</button>
+                            <button className={styles.submit}>submit</button>
+    
+                        </footer>
+                    </div>
                 </div>
             </div>
-        </div>
-        
-        : null}</>;
+            
+            : null}</>, 
+            document.getElementById("modal")
+
+    );
         
 };
     
